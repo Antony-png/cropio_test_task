83c5946890283085a627ce6be38f26247f689253
Anton Zelenkov <anton.zelenkov3@gmail.com>
2025-07-17 22:30:00 +0300

    Add migrations and Dockerize project

---
+++ db/migrate/20250715085423_create_fields.rb
@@ -0,0 +1,10 @@
+class CreateFields < ActiveRecord::Migration[8.0]
+  def change
+    create_table :fields do |t|
+      t.string :name
+      t.multi_polygon :shape, srid: 4326, geographic: true
+      t.float :area
+      t.timestamps
+    end
+  end
+end
